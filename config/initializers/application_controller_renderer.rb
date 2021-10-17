0a0306891965db6bd6c75e63a72685de588bc68e
wak10 <taka.watagashi422@gmail.com>
2021-10-17 10:01:42 +0000

    [add]画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b7d23de8982a95a46daf7517fb9bc4826ec41f43e98a0595cb63f813946feddf4a20e575210904692b479b0a6fd6950d8936442e2e6d01ea6e116d5dfe8e8508'
